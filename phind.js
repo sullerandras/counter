1d92ccdce2ba78c5df2eca7f0733ece8002a3522
Andras Suller <suller.andras@gmail.com>
2023-12-16 21:39:29 +0700

    Asked to make it mobile friendly but it isn't
    ```
    Can you make it mobile friendly and align the sections vertically?
    ```

--- phind.js
+++ phind.js
@@ -1,17 +1,7 @@
-document.getElementById('section1').addEventListener('click', function(e) {
-  if (e.target !== e.currentTarget) return; // Ignore clicks on the label
-  var counter = document.getElementById('counter1');
-  counter.textContent = parseInt(counter.textContent) + 1;
-});
-
-document.getElementById('section2').addEventListener('click', function(e) {
-  if (e.target !== e.currentTarget) return; // Ignore clicks on the label
-  var counter = document.getElementById('counter2');
-  counter.textContent = parseInt(counter.textContent) + 1;
-});
-
-document.getElementById('section3').addEventListener('click', function(e) {
-  if (e.target !== e.currentTarget) return; // Ignore clicks on the label
-  var counter = document.getElementById('counter3');
-  counter.textContent = parseInt(counter.textContent) + 1;
+document.body.addEventListener('click', function(e) {
+  if (e.target.classList.contains('section')) {
+      var counterId = 'counter' + e.target.id.charAt(e.target.id.length - 1);
+      var counter = document.getElementById(counterId);
+      counter.textContent = parseInt(counter.textContent) + 1;
+  }
 });
